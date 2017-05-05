7caede79d91306c7188df054ba49ee7c3af97d76
Cybis <cybis-fdp@hotmail.com>
2017-05-05 15:08:31 +0000

    [r32] I haz gots if statements meow! Also stuff from before / write and repeat commands were getting null reference error when trying to write/process literal values. (This is a small change to allow adding a comment. TSVN not setup to edit log comments).

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using CyBF.Parsing;
 using CyBF.BFC.Compilation;
 using CyBF.BFC.Model;
