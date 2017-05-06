daeb8f4f649fba8b77f63cba6234697b2a8d6249
Cybis <cybis-fdp@hotmail.com>
2017-05-06 01:28:11 +0000

    [r35] Added check for recursive procedures.

--- BFC/Model/Functions/ProcedureDefinition.cs
+++ BFC/Model/Functions/ProcedureDefinition.cs
@@ -12,6 +12,7 @@ namespace CyBF.BFC.Model.Functions
         public Token Reference { get; private set; }
         public IReadOnlyList<Statement> Body { get; private set; }
         public ExpressionStatement ReturnExpression { get; private set; }
+        public bool IsCompiling { get; private set; }
 
         public ProcedureDefinition(
             Token reference,
@@ -24,20 +25,28 @@ namespace CyBF.BFC.Model.Functions
             this.Reference = reference;
             this.Body = body.ToList().AsReadOnly();
             this.ReturnExpression = returnExpression;
+            this.IsCompiling = false;
         }
 
         public override BFObject Compile(BFCompiler compiler, IEnumerable<BFObject> arguments)
         {
             compiler.TracePush(this.Reference);
-            
+
+            if (this.IsCompiling)
+                compiler.RaiseSemanticError("Recursive functions are not supported.");
+
             this.ApplyArguments(compiler, arguments);
+
+            this.IsCompiling = true;
 
             foreach (Statement statement in this.Body)
                 statement.Compile(compiler);
 
             this.ReturnExpression.Compile(compiler);
             BFObject returnValue = this.ReturnExpression.ReturnVariable.Value;
-            
+
+            this.IsCompiling = false;
+
             compiler.TracePop();
 
             return returnValue;
